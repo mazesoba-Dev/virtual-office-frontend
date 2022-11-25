dc20ad82a30d6b8661de3e1ddce7b495fc110539
JUNoym <junnyaooyama@gmail.com>
2022-11-26 00:29:15 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+      名前を入力してから入れるページ
+    </div>
+  )
+}
---
+++ pages/name.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <div>
+        ここで名前を入力する
+    </div>
+  )
+}
